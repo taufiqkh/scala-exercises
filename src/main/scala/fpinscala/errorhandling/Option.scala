5255dd7eaa1b1c3f2d8ad0eacb4c2e627e4b263c
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-11-11 09:26:02 +1000

    Added Option exercise from chapter 4

---
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -0,0 +1,26 @@
+package fpinscala.errorhandling
+
+/**
+  * Chapter 4, Functional Programming in Scala
+  */
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+  def orElse[B >: A](ob: Option[B]): Option[B] =
+    map(Some(_)).getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] = {
+    flatMap(a => if (f(a)) Some(a) else None)
+  }
+}
+case class Some[+A](get: A) extends Option[A]
+
+case object None extends Option[Nothing]
