71fb128d8ebb90e402fffc67cb8980bd6a86c97c
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-11-11 12:45:07 +1000

    Exercises up to 4.4

--- src/main/scala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -14,13 +14,34 @@ sealed trait Option[+A] {
     case None => default
     case Some(a) => a
   }
-  def orElse[B >: A](ob: Option[B]): Option[B] =
-    map(Some(_)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = {
-    flatMap(a => if (f(a)) Some(a) else None)
-  }
+  def orElse[B >: A](ob: Option[B]): Option[B] = map(Some(_)).getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if (f(a)) Some(a) else None)
 }
 case class Some[+A](get: A) extends Option[A]
-
 case object None extends Option[Nothing]
+
+object Option {
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap((m) => mean(xs.map(x => math.pow(x - m, 2))))
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap((aa) => b.map((bb) => f(aa, bb)))
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t =>
+      h.flatMap((hh) => sequence(t).map(hh :: _))
+  }
+
+  def sequence2[A](a: List[Option[A]]): Option[List[A]] = {
+    a.foldRight[Option[List[A]]](Some(Nil))((x, tail) => map2(x, tail)(_ :: _))
+  }
+}
