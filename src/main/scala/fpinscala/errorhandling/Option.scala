32665b83bf79ace9e293fe08f19a0583f5e5a936
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-11-15 08:31:43 +1000

    4.5 updated to use for comprehension

--- src/main/scala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -33,7 +33,10 @@ object Option {
     mean(xs).flatMap((m) => mean(xs.map(x => math.pow(x - m, 2))))
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-    a.flatMap((aa) => b.map((bb) => f(aa, bb)))
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa, bb)
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
     case Nil => Some(Nil)
@@ -48,6 +51,9 @@ object Option {
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
     case Nil => Some(Nil)
     case h :: t =>
-      f(h).flatMap((hh) => traverse(t)(f).map(hh :: _))
+      for {
+        hh <- f(h)
+        rest <- traverse(t)(f)
+      } yield hh :: rest
   }
 }
